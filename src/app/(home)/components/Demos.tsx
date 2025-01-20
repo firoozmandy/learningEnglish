99f9bf3ee0b4261e0b398419ef9f441e80eb6377
firoozmandy <maryam.firoozmandi@gmail.come>
2025-01-20 16:21:37 +0330

    target

--- src/app/(home)/components/Demos.tsx
+++ src/app/(home)/components/Demos.tsx
@@ -4,15 +4,15 @@ import { pricingData } from '../data'
 const Demos = () => {
   return (
     <section className="section scroll-mt-5 pb-16" id="demos">
-    
       <div className="mx-14">
         <div className="mb-10 text-center text-2xl font-semibold">
-        Learn with one of our popular online courses        </div>
+          Learn with one of our popular online courses{' '}
+        </div>
         <div className="grid justify-center gap-4 lg:grid-cols-3">
           {pricingData.map((item, idx) => {
             return (
               <div className="mx-auto px-4" key={idx}>
-                <Link to={item.path} target="_blank">
+                <Link to={item.path} target="_parent">
                   <img
                     src={item.image}
                     className="rounded border border-gray-100 bg-white p-2 shadow-lg"
